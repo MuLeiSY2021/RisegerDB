356a391d9bb4a40d0ec80979fa9f2b24ca0104d5
MuLei_SY <1552895940@qq.com>
2023-08-24 02:42:12 -0600

    [feat]优化了R树，完善了R树对于浮点坐标精确性的范围划定

--- model/JRDBC_Protocol/src/main/java/org/riseger/jrdbc/protocol/HeartBeatResponsePacket.java
+++
@@ -1,8 +0,0 @@
-package org.riseger.jrdbc.protocol;
-
-public class HeartBeatResponsePacket extends BaseMsgPacket{
-    @Override
-    public byte getCommand() {
-        return Constant.HEART_BEAT_RESPONSE;
-    }
-}
